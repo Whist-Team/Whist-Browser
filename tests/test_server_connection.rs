5d73c8bdc938512f9a6b58b3fac8b69c3b5867f9
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-09 01:32:08 +0200

    UPDATE: use mocks for server
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/test_server_connection.rs
+++ tests/test_server_connection.rs
@@ -1,13 +1,24 @@
 #[cfg(test)]
 mod test_server_connection {
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+    use wiremock::matchers::method;
     use whist_browser::response::whist_info::{WhistInfo, WhistInfoFactory};
     use whist_browser::server_connection::get_json;
 
     #[tokio::test]
     async fn test_get_json() {
-        let url = "http://localhost:9001";
-        let response_json = get_json::<WhistInfo>(&url).await.unwrap();
         let expected_info = WhistInfoFactory::new_info("whist", "0.1.0");
+
+        let mock_server = MockServer::start().await;
+        Mock::given(method("GET"))
+            .respond_with(ResponseTemplate::new(200)
+                .set_body_json(expected_info.clone()))
+            .mount(&mock_server)
+            .await;
+        let response_json = get_json::<WhistInfo>(
+            &mock_server.uri())
+            .await
+            .unwrap();
         assert_eq!(response_json, expected_info);
     }
 }
