52101605666e93fdbb9cdc03804cde3eaf86432f
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-09 01:50:41 +0200

    ADD: documentation
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- src/response/whist_info.rs
+++ src/response/whist_info.rs
@@ -1,24 +1,36 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Inner layer containing a details.
 struct GameInfo {
+    ///name of the current supported game
     game: String,
+    ///version of the Whist-Core
     version: String,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Outer layer of the info object return from Whist-Server
 pub struct WhistInfo {
+    /// inner layer containing all server details
     info: GameInfo,
 
 }
+
+/// Factory for the Whist Info structure
 pub struct WhistInfoFactory;
 
+/// Implementation of the factory
+/// # Arguments
+///
+/// * 'game' - The name of the game support
+/// * 'version' - The core version of the above game.
 impl WhistInfoFactory {
-    pub fn new_info(game: &str, version: &str) -> WhistInfo{
+    pub fn new_info(game: &str, version: &str) -> WhistInfo {
         return WhistInfo {
             info: GameInfo {
                 game: game.to_owned(),
-                version: version.to_owned()
+                version: version.to_owned(),
             }
         };
     }
--- src/server_connection.rs
+++ src/server_connection.rs
@@ -1,6 +1,8 @@
 use reqwest::Error;
 use serde::de::DeserializeOwned;
-
+/// Requests a GET route and transforms it to a JSON object.
+/// # Arguments
+/// * 'url' - The full uri to the requested route include optional path variables.
 pub async fn get_json<T: DeserializeOwned>(url: &str) -> Result<T, Error> {
     Ok(reqwest::get(url)
         .await?
