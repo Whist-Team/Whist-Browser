5cc0e4fafdfd94d1dd692c858383e015be3a278d
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-09 22:08:19 +0200

    UPDATE: move unit tests to module
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- src/server_connection.rs
+++ src/server_connection.rs
@@ -39,3 +39,44 @@ impl ServerConnection {
         Ok(response)
     }
 }
+#[cfg(test)]
+mod tests {
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+    use wiremock::matchers::method;
+
+    use crate::response::whist_info::{WhistInfo, WhistInfoFactory};
+    use crate::server_connection::ServerConnection;
+
+    #[tokio::test]
+    async fn test_get_json() {
+        let expected_info =
+            WhistInfoFactory::new_info(String::from("whist"), String::from("0.1.0"));
+
+        let mock_server = MockServer::start().await;
+        Mock::given(method("GET"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(expected_info.clone()))
+            .mount(&mock_server)
+            .await;
+        let conn = ServerConnection::new(mock_server.uri() + "/route");
+        let response_json = conn.get_json::<WhistInfo>("/route").await.unwrap();
+        assert_eq!(response_json, expected_info);
+    }
+
+    #[tokio::test]
+    async fn test_post_json_without_response_body() {
+        let expected_info =
+            WhistInfoFactory::new_info(String::from("whist"), String::from("0.1.0"));
+
+        let mock_server = MockServer::start().await;
+        Mock::given(method("POST"))
+            .respond_with(ResponseTemplate::new(200))
+            .mount(&mock_server)
+            .await;
+        let conn = ServerConnection::new(mock_server.uri() + "/route");
+        let response_json = conn
+            .post_json::<WhistInfo>("/route", expected_info)
+            .await
+            .unwrap();
+        assert_eq!(response_json.status(), 200);
+    }
+}
