54b5c3cf0811d02abba9a28c97d24d67b52b5c73
iTitus <iTitus@users.noreply.github.com>
2022-06-21 22:01:27 +0200

    add some tests

--- src/network/game.rs
+++ src/network/game.rs
@@ -28,10 +28,90 @@ pub enum GameJoinStatus {
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct GameJoinResponse {
-    pub status: Option<String>,
+    pub status: GameJoinStatus,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct GameCreateResponse {
     pub game_id: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use crate::network::{GameCreateRequest, GameJoinResponse, GameJoinStatus};
+
+    #[test]
+    fn test_game_join_response_deserialize_1() {
+        let expected = GameJoinResponse {
+            status: GameJoinStatus::Joined,
+        };
+        let actual: GameJoinResponse = serde_json::from_value(json!({"status": "joined"})).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_game_join_response_deserialize_2() {
+        let expected = GameJoinResponse {
+            status: GameJoinStatus::AlreadyJoined,
+        };
+        let actual: GameJoinResponse =
+            serde_json::from_value(json!({"status": "already joined"})).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_game_create_request_serialize_1() {
+        let expected = json!({
+            "game_name": "asdf",
+            "password": null,
+            "min_player": null,
+            "max_player": null,
+        });
+        let actual = serde_json::to_value(&GameCreateRequest {
+            game_name: "asdf".to_string(),
+            password: None,
+            min_player: None,
+            max_player: None,
+        })
+        .unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_game_create_request_serialize_2() {
+        let expected = json!({
+            "game_name": "asdf",
+            "password": "12345_is_a_bad_password",
+            "min_player": null,
+            "max_player": null,
+        });
+        let actual = serde_json::to_value(&GameCreateRequest {
+            game_name: "asdf".to_string(),
+            password: Some("12345_is_a_bad_password".to_string()),
+            min_player: None,
+            max_player: None,
+        })
+        .unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_game_create_request_serialize_3() {
+        let expected = json!({
+            "game_name": "asdf",
+            "password": "12345_is_a_bad_password",
+            "min_player": 4,
+            "max_player": 4,
+        });
+        let actual = serde_json::to_value(&GameCreateRequest {
+            game_name: "asdf".to_string(),
+            password: Some("12345_is_a_bad_password".to_string()),
+            min_player: Some(4),
+            max_player: Some(4),
+        })
+        .unwrap();
+        assert_eq!(expected, actual);
+    }
 }
