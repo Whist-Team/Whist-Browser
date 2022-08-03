11bda0ca6afbeb7fb0a884064500c6c1c152f1bb
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-08-03 23:34:56 +0200

    FIX: range
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- src/network/github.rs
+++ src/network/github.rs
@@ -16,8 +16,26 @@ impl GitHubAuthRequest {
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct GitHubTempTokenResponse {
     pub device_code: String,
-    pub expires_in: u8,
+    pub expires_in: i32,
     pub interval: u8,
     pub user_code: String,
     pub verification_uri: String,
+}
+
+impl GitHubTempTokenResponse {
+    pub fn new(
+        device_code: impl Into<String>,
+        expires_in: impl Into<i32>,
+        interval: impl Into<u8>,
+        user_code: impl Into<String>,
+        verification_uri: impl Into<String>,
+    ) -> Self {
+        Self {
+            device_code: device_code.into(),
+            expires_in: expires_in.into(),
+            interval: interval.into(),
+            user_code: user_code.into(),
+            verification_uri: verification_uri.into(),
+        }
+    }
 }
