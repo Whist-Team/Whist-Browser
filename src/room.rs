b31a091abb57d06e24d693488f75cffc9627bb5d
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-09-04 23:25:41 +0200

    UPDATE: ui to display info
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- src/room.rs
+++ src/room.rs
@@ -1,8 +1,10 @@
-use bevy::prelude::ResMut;
+use crate::network::{RoomInfoResponse, RoomInfoResult};
+use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext};
 
 #[derive(Debug)]
 enum RoomStatus {
+    Error(String),
     WaitingForPlayers,
     ReadyToStart,
     Starting,
@@ -17,6 +19,15 @@ impl RoomStatus {
 
 struct UiState {
     room_status: RoomStatus,
+    name: String,
+    password: bool,
+    rubber_number: u8,
+    game_number: u8,
+    hand_number: u8,
+    trick_number: u8,
+    min_player: u8,
+    max_player: u8,
+    player_number: u8,
 }
 
 impl UiState {
@@ -32,11 +43,40 @@ impl Default for UiState {
     fn default() -> Self {
         Self {
             room_status: RoomStatus::WaitingForPlayers,
+            name: "".to_string(),
+            password: false,
+            rubber_number: 0,
+            game_number: 0,
+            hand_number: 0,
+            trick_number: 0,
+            min_player: 0,
+            max_player: 0,
+            player_number: 0,
         }
     }
 }
 
-fn update_ui_state(mut ui_state: ResMut<UiState>) {}
+fn update_ui_state(
+    mut ui_state: ResMut<UiState>,
+    mut room_info_results: EventReader<RoomInfoResult>,
+) {
+    if let Some(room_info_result) = room_info_results.iter().next_back() {
+        match room_info_result {
+            Ok(room_info) => {
+                ui_state.name = room_info.name.to_owned();
+                ui_state.password = room_info.password.to_owned();
+                ui_state.rubber_number = room_info.rubber_number.to_owned();
+                ui_state.game_number = room_info.game_number.to_owned();
+                ui_state.hand_number = room_info.hand_number.to_owned();
+                ui_state.trick_number = room_info.trick_number.to_owned();
+                ui_state.min_player = room_info.min_player.to_owned();
+                ui_state.max_player = room_info.max_player.to_owned();
+                ui_state.player_number = room_info.player_number.to_owned();
+            }
+            Err(e) => ui_state.room_status = RoomStatus::Error(format!("{:?}", e)),
+        }
+    }
+}
 
 fn lobby_menu(mut egui_context: ResMut<EguiContext>, mut ui_state: ResMut<UiState>) {
     let ui_state: &mut UiState = &mut ui_state;
