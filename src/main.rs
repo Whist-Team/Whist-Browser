1e5a4ac8752bc17b219369d73d8e8356e846625f
iTitus <iTitus@users.noreply.github.com>
2022-11-23 11:15:31 +0100

    only enable asset reload on non wasm

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,25 @@ use whist_browser::GamePlugin;
 
 #[bevy_main]
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                title: "Whist".to_string(),
-                width: 800.0,
-                height: 600.0,
-                ..default()
-            },
+    let mut plugins = DefaultPlugins.build();
+    plugins = plugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            title: "Whist".to_string(),
+            width: 800.0,
+            height: 600.0,
             ..default()
-        }))
-        .add_plugin(GamePlugin)
-        .run();
+        },
+        ..default()
+    });
+
+    #[cfg(not(target_family = "wasm"))]
+    {
+        println!("Enabling filesystem watcher for asset reload");
+        plugins = plugins.set(AssetPlugin {
+            watch_for_changes: true,
+            ..default()
+        });
+    }
+
+    App::new().add_plugins(plugins).add_plugin(GamePlugin).run();
 }
