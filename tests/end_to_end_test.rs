621e026f99a2a43825a16fceeb281022745b74d5
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-28 23:29:27 +0100

    ADD: integration test
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

---
+++ tests/end_to_end_test.rs
@@ -0,0 +1,8 @@
+use whist_browser;
+
+#[tokio::test]
+async fn test_start() {
+    let service = whist_browser::network::ServerService::new("http://localhost:8080");
+    let whist_info = service.get_info().await.unwrap();
+    assert_eq!(whist_info.info.game, "whist");
+}
