247fcad9cda33d89ba917d759a474b49dcd2853a
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-29 18:33:34 +0100

    ADD: check connection test
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/end_to_end_test.rs
+++ tests/end_to_end_test.rs
@@ -2,6 +2,14 @@
 #[ignore]
 async fn test_start() {
     let service = whist_browser::network::ServerService::new("http://localhost:8080");
+    let response = service.check_connection().await;
+    assert_eq!(true, response.is_ok());
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_game_info() {
+    let service = whist_browser::network::ServerService::new("http://localhost:8080");
     let whist_info = service.get_info().await.unwrap();
     assert_eq!(whist_info.info.game, "whist");
 }
