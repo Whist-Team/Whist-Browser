9e4b1b1453a2c71b5793673813d8b34fac60f12c
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2022-11-29 21:07:03 +0100

    Fix assertion

--- tests/end_to_end_test.rs
+++ tests/end_to_end_test.rs
@@ -5,7 +5,7 @@ use whist_browser::network::LoginForm;
 async fn test_start() {
     let service = whist_browser::network::ServerService::new("http://localhost:8080");
     let response = service.check_connection().await;
-    assert!(true, response.is_ok());
+    assert!(response.is_ok());
 }
 
 #[tokio::test]
@@ -22,7 +22,7 @@ async fn test_login() {
     let mut service = whist_browser::network::ServerService::new("http://localhost:8080");
     let login_form = LoginForm::new("root", "password");
     let response = service.login(&login_form).await;
-    assert!(true, response.is_ok());
+    assert!(response.is_ok());
 }
 
 #[tokio::test]
@@ -32,6 +32,6 @@ async fn test_get_games() {
     let login_form = LoginForm::new("root", "password");
     let _ = service.login(&login_form).await;
     let response = service.get_games().await;
-    assert!(true, response.is_ok());
+    assert!(response.is_ok());
     assert_eq!(Vec::<String>::new(), response.unwrap().rooms);
 }
