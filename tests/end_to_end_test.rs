b52df6f5da76c741117f23379c74b15ebe4a8b1a
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-29 18:42:04 +0100

    ADD: more integration tests
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/end_to_end_test.rs
+++ tests/end_to_end_test.rs
@@ -24,3 +24,14 @@ async fn test_login() {
     let response = service.login(&login_form).await;
     assert_eq!(true, response.is_ok());
 }
+
+#[tokio::test]
+#[ignore]
+async fn test_get_games() {
+    let mut service = whist_browser::network::ServerService::new("http://localhost:8080");
+    let login_form = LoginForm::new("root", "password");
+    let _ = service.login(&login_form).await;
+    let response = service.get_games().await;
+    assert_eq!(true, response.is_ok());
+    assert_eq!(Vec::<String>::new(), response.unwrap().rooms);
+}
