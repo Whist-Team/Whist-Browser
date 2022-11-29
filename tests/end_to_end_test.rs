8c4fe4248aacd396cc768e2fd0e29f493b7944e4
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-29 18:37:26 +0100

    ADD: test for login
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/end_to_end_test.rs
+++ tests/end_to_end_test.rs
@@ -1,3 +1,5 @@
+use whist_browser::network::LoginForm;
+
 #[tokio::test]
 #[ignore]
 async fn test_start() {
@@ -12,4 +14,13 @@ async fn test_game_info() {
     let service = whist_browser::network::ServerService::new("http://localhost:8080");
     let whist_info = service.get_info().await.unwrap();
     assert_eq!(whist_info.info.game, "whist");
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_login() {
+    let mut service = whist_browser::network::ServerService::new("http://localhost:8080");
+    let login_form = LoginForm::new("root", "password");
+    let response = service.login(&login_form).await;
+    assert_eq!(true, response.is_ok());
 }
